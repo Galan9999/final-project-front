dece7fb5b7a8ed9b8d457c6ef56b01788f01b1fb
Carles Galan <galan.carles@gmail.com>
2023-03-14 21:31:02 +0100

    Refactor all router imports

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,17 +1,34 @@
 import { screen } from "@testing-library/react";
+import { preloadedIsLoadingUiState } from "../../mocks/uiPreloadedStates";
 import renderWithRouters from "../../utils/testUtils/renderWithRouters";
 import Layout from "../Layout/Layout";
+import Loader from "../Loader/Loader";
 
 describe("Given the Layout component", () => {
   describe("When rendered", () => {
     test("Then it should show a header with the logo of 'sentio'", () => {
       const expectedText = "logo";
 
-      renderWithRouters(<Layout />);
+      renderWithRouters({ ui: <Layout /> });
 
       const ariaLabelText = screen.getByLabelText(expectedText);
 
       expect(ariaLabelText).toBeInTheDocument();
+    });
+  });
+
+  describe("When the page is loading", () => {
+    test("Then it should show the loading animation", () => {
+      const expectedLabelText = "the app is loading";
+
+      renderWithRouters({
+        ui: <Loader />,
+        preloadedState: preloadedIsLoadingUiState,
+      });
+
+      const loader = screen.getByRole("dialog", { name: expectedLabelText });
+
+      expect(loader).toBeInTheDocument();
     });
   });
 });
