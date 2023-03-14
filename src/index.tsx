f0e97d51f1421e8b6be784bfcef9c6bce8fba261
Carles Galan <galan.carles@gmail.com>
2023-03-14 10:16:56 +0100

    Implement router

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import App from "./components/App/App";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import GlobalStyles from "./styles/GlogablStyles";
+import { RouterProvider } from "react-router";
+import { router } from "./router/router";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -13,9 +14,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
       <Provider store={store}>
-        <App />
+        <GlobalStyles />
+        <RouterProvider router={router} />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
