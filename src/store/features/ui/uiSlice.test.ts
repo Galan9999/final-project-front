287377ef8bbfe17359528f8ad2380add0f3dc74d
Carles Galan <galan.carles@gmail.com>
2023-03-14 09:41:36 +0100

    Add modal actions to reducer

--- src/store/features/ui/uiSlice.test.ts
+++ src/store/features/ui/uiSlice.test.ts
@@ -1,7 +1,9 @@
 import { UiStructure } from "../../../types";
 import {
+  setIsErrorModalActionCreator,
   setIsLoadingActionCreator,
   uiReducer,
+  unsetIsErrorModalActionCreator,
   unsetIsLoadingActionCreator,
 } from "./uiSlice";
 
@@ -10,9 +12,17 @@ describe("Given the uiSlice reducer function", () => {
     test("Then it should set isLoading to true", () => {
       const currentUiState: UiStructure = {
         isLoading: false,
+        modal: {
+          isError: false,
+          message: "",
+        },
       };
       const expectedUiState: UiStructure = {
         isLoading: true,
+        modal: {
+          isError: false,
+          message: "",
+        },
       };
       const newUiState = uiReducer(currentUiState, setIsLoadingActionCreator());
 
@@ -22,14 +32,76 @@ describe("Given the uiSlice reducer function", () => {
 
   describe("When it receives the unsetIsLoading action", () => {
     test("Then it should set isLoading to false", () => {
-      const currentUi: UiStructure = {
+      const currentUiState: UiStructure = {
         isLoading: true,
+        modal: {
+          isError: false,
+          message: "",
+        },
       };
       const expectedUiState: UiStructure = {
         isLoading: false,
+        modal: {
+          isError: false,
+          message: "",
+        },
       };
 
-      const newUiState = uiReducer(currentUi, unsetIsLoadingActionCreator());
+      const newUiState = uiReducer(
+        currentUiState,
+        unsetIsLoadingActionCreator()
+      );
+
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+
+  describe("When it receives the setErrorModal action", () => {
+    test("Then it should set isErrorModal to true with error message", () => {
+      const currentUiState: UiStructure = {
+        isLoading: true,
+        modal: {
+          isError: false,
+          message: "",
+        },
+      };
+      const expectedUiState: UiStructure = {
+        isLoading: true,
+        modal: {
+          isError: true,
+          message: "error message",
+        },
+      };
+
+      const newUiState = uiReducer(
+        currentUiState,
+        setIsErrorModalActionCreator(expectedUiState.modal.message)
+      );
+
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+  describe("When it receives the unsetModal action", () => {
+    test("Then it should set isModal to false", () => {
+      const currentUiState: UiStructure = {
+        isLoading: true,
+        modal: {
+          isError: false,
+          message: "",
+        },
+      };
+      const expectedUiState: UiStructure = {
+        isLoading: true,
+        modal: {
+          isError: false,
+          message: "",
+        },
+      };
+
+      const newUiState = uiReducer(
+        currentUiState,
+        unsetIsErrorModalActionCreator()
+      );
 
       expect(newUiState).toStrictEqual(expectedUiState);
     });
