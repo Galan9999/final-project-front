e9572bc4ec0f030f14f150ae1756a43cf1fa85fe
Carles Galan <galan.carles@gmail.com>
2023-03-15 00:42:27 +0100

    Test reducer case of logout action

--- src/store/features/user/userSlice.test.ts
+++ src/store/features/user/userSlice.test.ts
@@ -1,7 +1,11 @@
 import { UserState } from "../../../types";
-import { loginUserActionCreator, userReducer } from "./userSlice";
+import {
+  loginUserActionCreator,
+  logoutUserActionCreator,
+  userReducer,
+} from "./userSlice";
 
-describe("Given the loginUser reducer function", () => {
+describe("Given the User reducer function", () => {
   describe("When it receives current state and loginUserAction", () => {
     test("Then it should return the user with token: 'iu23' and property isLogged: true", () => {
       const currentUser: UserState = {
@@ -17,6 +21,22 @@ describe("Given the loginUser reducer function", () => {
         currentUser,
         loginUserActionCreator("holitoken")
       );
+
+      expect(newUserState).toStrictEqual(expetedUserState);
+    });
+  });
+  describe("When it receives a logoutUserAction", () => {
+    test("Then it should return a user without token and property isLogged to false", () => {
+      const currentUser: UserState = {
+        token: "holitoken",
+        isLogged: true,
+      };
+      const expetedUserState: UserState = {
+        token: "",
+        isLogged: false,
+      };
+
+      const newUserState = userReducer(currentUser, logoutUserActionCreator());
 
       expect(newUserState).toStrictEqual(expetedUserState);
     });
