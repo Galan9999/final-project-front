4af22469e2d8426b628c55d2c86d362dcbe35682
Carles Galan <galan.carles@gmail.com>
2023-03-15 22:28:51 +0100

    Test loadQuotes on reducer

---
+++ src/store/features/quotes/quotesSlice.test.ts
@@ -0,0 +1,41 @@
+import { QuotesStructure } from "../../../types";
+import { loadQuotesActionCreator, quotesReducer } from "./quotesSlice";
+
+describe("Given the quotes reducer function", () => {
+  describe("When its called with loadQuotes action", () => {
+    test("Then it should return a list of quotes", () => {
+      const currentQuotesState: QuotesStructure = [
+        {
+          author: "",
+          image: "",
+          country: "",
+          quote: "",
+          tags: [""],
+          lived: "",
+          backgroundInfo: "",
+        },
+      ];
+      const expectedQuotesState: QuotesStructure = [
+        {
+          author: "Albert Einstein",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/440px-Albert_Einstein_Head.jpg",
+          country: "Germany",
+          quote:
+            "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
+          tags: ["philosophers"],
+          lived: "1879 - 1955",
+          backgroundInfo:
+            "Albert Einstein was a German-born theoretical physicist who developed the theory of relativity, one of the two pillars of modern physics.",
+        },
+      ];
+
+      const newQuotesState = quotesReducer(
+        currentQuotesState,
+        loadQuotesActionCreator(expectedQuotesState)
+      );
+
+      expect(newQuotesState).toStrictEqual(expectedQuotesState);
+    });
+  });
+});
