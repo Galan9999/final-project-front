ca2e18de3fb872ef5c537a67c38085e7050d73f0
Carles Galan <galan.carles@gmail.com>
2023-03-17 20:05:08 +0100

    Add Home Page to router

--- src/mocks/quotesPreloadedStates.ts
+++ src/mocks/quotesPreloadedStates.ts
@@ -56,23 +56,29 @@ export const mockedPreloadeStoreLoggedState: StoreStructure = {
   quotes: [
     {
       id: "1",
-      author: "Catalina",
-      image: "",
-      country: "",
-      quote: "",
-      tags: [""],
-      lived: "",
-      backgroundInfo: "",
+      author: "Barack Obama",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/440px-President_Barack_Obama.jpg",
+      country: "United States",
+      quote:
+        "Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.",
+      tags: ["politics"],
+      lived: "1961 - present",
+      backgroundInfo:
+        "Barack Obama is an American politician and attorney who served as the 44th president of the United States from 2009 to 2017.",
     },
     {
       id: "2",
-      author: "Carles",
-      image: "",
-      country: "",
-      quote: "",
-      tags: [""],
-      lived: "",
-      backgroundInfo: "",
+      author: "Albert Einstein",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/440px-Albert_Einstein_Head.jpg",
+      country: "Germany",
+      quote:
+        "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
+      tags: ["philosophers"],
+      lived: "1879 - 1955",
+      backgroundInfo:
+        "Albert Einstein was a German-born theoretical physicist who developed the theory of relativity, one of the two pillars of modern physics.",
     },
   ],
 };
