91c1997c442b50dc4b299f1815821a2fbb65ccd5
Carles Galan <galan.carles@gmail.com>
2023-03-18 08:57:18 +0100

    Test UnprotectedRoutes component

--- src/components/UnprotectedRoutes/UnprotectedRoutes.test.tsx
+++ src/components/UnprotectedRoutes/UnprotectedRoutes.test.tsx
@@ -1,19 +1,19 @@
-import ProtectedRoutes from "./ProtectedRoutes";
+import UnprotectedRoutes from "./UnprotectedRoutes";
 import renderRouterWithProviders, {
   RouterRenderOptions,
 } from "../../utils/testUtils/renderRouterWithProviders";
 import { screen } from "@testing-library/react";
 
-describe("Given the ProtectedRoutes component", () => {
-  describe("When a logged user with token and rendered a container with text `Loren Ipsum`", () => {
+describe("Given the UnprotectedRoutes component", () => {
+  describe("When a not logged user without token and rendered a container with text `Loren Ipsum`", () => {
     test("Then it should show the container with text `Loren Ispum`", () => {
       const text = "Loren Ipsum";
       const containerWithText = <div>{text}</div>;
 
       const routerAndState: RouterRenderOptions = {
-        ui: <ProtectedRoutes children={containerWithText} />,
+        ui: <UnprotectedRoutes children={containerWithText} />,
         preloadedState: {
-          user: { isLogged: true, token: "token" },
+          user: { isLogged: false, token: "" },
         },
       };
 
@@ -24,15 +24,15 @@ describe("Given the ProtectedRoutes component", () => {
       expect(expectedRenderedText).toBeInTheDocument();
     });
   });
-  describe("When it is rendered with a container with text `Loren Ipsum` and user without token", () => {
+  describe("When it is rendered with a container with text `Loren Ipsum` and logged user with token", () => {
     test("Then it shoudn't show the container with text `Loren Ipsum`", () => {
       const text = "Loren Ipsum";
       const containerWithText = <div>{text}</div>;
 
       const routerAndState: RouterRenderOptions = {
-        ui: <ProtectedRoutes children={containerWithText} />,
+        ui: <UnprotectedRoutes children={containerWithText} />,
         preloadedState: {
-          user: { isLogged: false, token: "" },
+          user: { isLogged: true, token: "token" },
         },
       };
 
